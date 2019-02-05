bc2356ad4b01c9f8881c6142c080c069abee9c2a
AnishaMurugan <43604574+AnishaMurugan@users.noreply.github.com>
2019-02-05 15:19:59 +0530

    Rename num.py to palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+ int main()
+ {
+    int n, reverse = 0, t;
+    printf("Enter a number to check if it is a palindrome or not\n");
+    scanf("%d", &n);
+    t = n;
+    while (t != 0)
+  }
